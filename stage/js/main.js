74ad6dc148f49fe589929d098b7eca73357479a4
Khaleedabuhawwas92 <42714078+Khaleedabuhawwas92@users.noreply.github.com>
2020-06-24 14:48:51 +0300

    Full Screen Funcation

--- stage/js/main.js
+++ stage/js/main.js
@@ -7,7 +7,50 @@ $(function () {
 
    //toggle Submune
    $(".toggle-submune").on("click", function () {
-      $(this).find(".icon-angle-right").toggleClass('down');
+      $(this).find(".icon-angle-right").toggleClass("down");
       $(this).next(".child-links").slideToggle();
    });
+
+   // Full Screen  Funcation
+   $(".toggle-fullscreen").on("click", function () {
+      $(this).toggleClass("full-screen");
+      if ($(this).hasClass("full-screen")) {
+         openFullscreen();
+      } else {
+         closeFullscreen();
+      }
+   });
 });
+
+var elem = document.documentElement;
+/* View in fullscreen */
+function openFullscreen() {
+   if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+   } else if (elem.mozRequestFullScreen) {
+      /* Firefox */
+      elem.mozRequestFullScreen();
+   } else if (elem.webkitRequestFullscreen) {
+      /* Chrome, Safari and Opera */
+      elem.webkitRequestFullscreen();
+   } else if (elem.msRequestFullscreen) {
+      /* IE/Edge */
+      elem.msRequestFullscreen();
+   }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+   if (document.exitFullscreen) {
+      document.exitFullscreen();
+   } else if (document.mozCancelFullScreen) {
+      /* Firefox */
+      document.mozCancelFullScreen();
+   } else if (document.webkitExitFullscreen) {
+      /* Chrome, Safari and Opera */
+      document.webkitExitFullscreen();
+   } else if (document.msExitFullscreen) {
+      /* IE/Edge */
+      document.msExitFullscreen();
+   }
+}
